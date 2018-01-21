44aff67c8929f28b2368757bc13ff2f9df0329f5
Akul Goyal <akgoyal@ucsc.edu>
2018-01-21 00:01:30 -0800

    made some changes to the database

--- c2c_server/database_manager.py
+++ c2c_server/database_manager.py
@@ -10,13 +10,14 @@
 import sqlite3
 import json
 import sys
+import math
 
 
 # Strings that contain sqlite execution instructions
-insert_user = "INSERT OR REPLACE INTO user_profiles VALUES (?, ?, ?, ?)"
+# insert_user = "INSERT OR REPLACE INTO user_profiles VALUES (?, ?, ?, ?)"
 insert_location = "INSERT INTO locations VALUES (?, ?, ?, ?, ?, ?)"
 insert_message = "INSERT INTO messages VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-load_users = "SELECT * FROM user_profiles"
+# load_users = "SELECT * FROM user_profiles"
 load_locations = "SELECT * FROM locations"
 load_messages = "SELECT * FROM messages"
 
@@ -33,18 +34,18 @@ class Database(object):
     #---SAVE--------------------------------------------------------------------
 
     # insert list of info into user_profiles
-    def save_json_to_users(self, json_package):
-        info = self.format_user_info(json_package)
-        self.database.execute(insert_user, info)
-
-    # takes a user json_package and converts it into a list to save to db
-    def format_user_info(self, json_package):
-        info = []
-        info.append(json_package["plate_num"])
-        info.append(json_package["car_type"])
-        info.append(json_package["car_color"])
-        info.append(json_package["name"])
-        return info
+    # def save_json_to_users(self, json_package):
+    #     info = self.format_user_info(json_package)
+    #     self.database.execute(insert_user, info)
+    #
+    # # takes a user json_package and converts it into a list to save to db
+    # def format_user_info(self, json_package):
+    #     info = []
+    #     info.append(json_package["plate_num"])
+    #     info.append(json_package["car_type"])
+    #     info.append(json_package["car_color"])
+    #     info.append(json_package["name"])
+    #     return info
 
     # insert list of info into locations
     def save_json_to_locations(self, json_package):
@@ -84,25 +85,131 @@ class Database(object):
         info.append(json_package["nearby_cars"])
         return info
 
-    #---LOAD--------------------------------------------------------------------
-
-    # load from user_profiles
-    def load_all_user_profiles(self):
-        result = self.database.execute(load_users)
-        print ("FILL IN CODE HERE")
-        sys.exit()
+    #---Helper--------------------------------------------------------------------
+    # user_profile={}
+    location_profile={}
+    message_profile={}
+    # # load from user_profiles
+    # def load_all_user_profiles(self):
+    #     result = self.database.execute(load_users)
+    #
+    #     # this is a dictionary of dictionary, go fuck your self if you
+    #     # cant undertsand it; you are a dumbass
+    #     for objects in result:
+    #         user_profile[objects[0]]={}
+    #         for elements in objects:
+    #             user_profile[objects[0]][elements]=elements
+    #
+    #     print ("FILL IN CODE HERE")
+    #     sys.exit()
 
     # load from locations
-    def load_all_locations(self):
-        result = self.database.execute(load_locations)
-        print ("FILL IN CODE HERE")
+
+
+    def load_in_location(self, json_package):
+        nameplate=json_package["plate_num"]
+        location_profile[nameplate]={}
+        for elements in json_package:
+            location_profile[nameplate][element]=elements
+
         sys.exit()
 
     # load from messages
-    def load_all_messages(self):
-        result = self.database.execute(load_messages)
-        print ("FILL IN CODE HERE")
+    def load_some_messages(self, json_package):
+        nameplate=json_package["plate_num"]
+        if message_profile[nameplate] == None:
+            message_profile[nameplate]={}
+        for elements in json_package:
+            if message_profile[nameplate][element]==None:
+                message_profile[nameplate][element]=[]
+                message_profile[nameplate][element].append(element)
+            else:
+                message_profile[nameplate][element]=message_profile[nameplate][element].append(elements)
+
         sys.exit()
+
+    def load_message_everyone(self, json_package):
+        for nameplate in location_profile:
+            if ((haversine(float(json_package["longitude"]),float(json_package["latitude"]),
+            float(nameplate["longitude"],float(nameplate["latitude"])))) < 61
+            and (floor_time(json_package["time"])) in floor_time(nameplate["send_time"])):
+                if message_profile[nameplate["plate_num"]]==None:
+                    message_profile[nameplate["plate_num"]]=[]
+                    message_profile[nameplate["plate_num"]]=
+                    message_profile[nameplate["plate_num"]].append(json_package["message"])
+                else:
+                    message_profile[nameplate["plate_num"]]=
+                    message_profile[nameplate["plate_num"]].append(json_package["message"])
+
+        sys.exit()
+
+    def floor_time(time):
+        timeS=int(time[-5:-3])
+        if timeS>=30:
+            timeM=int(time[-8:-6])
+            if timeM<59:
+                timeM=timeM+1
+                return time[0:-8] + str(timeM) + time[-6:len(time)]
+            else:
+                timeH=time[-11:-9]
+                if ' ' in timeH:
+                    timeH=timeH[1:2]
+                    flag=True
+                timeH=int(timeH)
+                timeH=timeH+1
+                if flag:
+                    return time[0:-10] + str(timeH)+':'+ str(00) + time[-6:len(time)]
+                else:
+                    return time[0:-11] + str(timeH)+':'+ str(00) + time[-6:len(time)]
+
+
+    def find_name_plate(self, color, cartype, longitude, latitude, nearby_cars, time):
+
+        if color is None:
+            return None
+
+        if cartype is None:
+            return None
+
+        # calc1=abs(float(longitude)-float(nameplate["longitude"]))<0.001352
+        # calc2=abs(float(latitude)-float(nameplate["latitude"]))<.000027
+        # calc3=abs(float(latitude)-float(nameplate["latitude"]))<0.001352
+        # calc4=abs(float(longitude)-float(nameplate["longitude"]))<.000027
+
+        for nameplate in nearby_cars:
+            if cartype in location_profile[nameplate]:
+                if color in location_profile[nameplate]:
+                    return nameplate["plate_num"]
+
+        for nameplate in location_profile:
+            if ((haversine(float(longitude),float(latitude),float(nameplate["longitude"],
+            float(nameplate["latitude"])))) < 61 and floor_time(time) in floor_time(nameplate["send_time"])):
+                    if cartype in nameplate and color in nameplate:
+                                return nameplate["plate_num"]
+        return None
+
+        def haversine(self, longitude1, latitude1, longitude2, latitude2):
+            lon1 = longitude1
+            lat1 = latitude1
+            lon2 = longitude1
+            lat2 = latitude2
+
+            R = 6371000  # radius of Earth in meters
+            phi_1 = math.radians(lat1)
+            phi_2 = math.radians(lat2)
+
+            delta_phi = math.radians(lat2 - lat1)
+            delta_lambda = math.radians(lon2 - lon1)
+
+            a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+            meters = R * c  # output distance in meters
+            return meters
+
+
+
 
     #---SET UP------------------------------------------------------------------
 
@@ -130,7 +237,7 @@ class Database(object):
         car_color TEXT,
         longitude REAL,
         latitude REAL,
-        send_time INT
+        send_time TEXT,
         nearby_cars TEXT);''')
         print ("Created locations table")
 
@@ -146,6 +253,6 @@ class Database(object):
         target_car_color TEXT,
         longitude REAL,
         latitude REAL,
-        send_time INT
+        send_time TEXT,
         nearby_cars TEXT);''')
         print ("Created messages table")
--- c2c_server/server.py
+++ c2c_server/server.py
@@ -19,38 +19,108 @@ from database_manager import Database
 db = Database()
 
 import sys
+import math
+
 
 #---REST API Routes-------------------------------------------------------------
 
 # user profile
-@app.route('/update_profile', methods = ['POST'])
-def update_profile():
-    db.save_json_to_users(dict(request.form))
+# @app.route('/update_profile', methods = ['POST'])
+# def update_profile():
+#     db.save_json_to_users(dict(request.form))
 
 
 # location
+
 @app.route('/update_location', methods = ['POST'])
 def update_location():
     db.save_json_to_locations(dict(request.form))
+    db.load_in_location(dict(request.form))
 
 
 # messages
+flag=False
+flag_error=False
 @app.route('/send_message', methods = ['POST'])
 def send_message():
-    info = process_message_info(dict(request.form))
-    db.save_json_to_messages(info)
+    info = check_everyone_present(dict(request.form))
+    if flag_error:
+        error_message="Car not found, please provide more information"
+        return error_message
+    if not flag:
+        db.save_json_to_messages(info)
+        db.load_some_messages(info)
+    else:
+        db.load_message_everyone(info)
+
+@app.route ('/recieve_message', methods=['POST'])
+def recieve_message():
+    info = (dict(request.form))['nameplate'] #this might not be right. Dont know how else to parse the request.
+    messages = db.get_messages_from_mailbox(info)
+    return messages
+
+
+
+    #delete message from mailbox
 
 
 #---Helpful Functions-----------------------------------------------------------
 
+def check_everyone_present(message):
+    if 'everyone' in message["message"]:
+        return process_message_for_everyone(message)
+        flag=True
+    else:
+        return process_message_info(message)
+
+
 # takes a message dict and adds missing info to it
+def process_message_for_everyone(message):
+    message["message"] = "Somone, 20 miles near you, said" + message["message"]
+    return message
+
 def process_message_info(message):
-    sys.exit()
-    message["target_plate_num"] = "CHANGE THIS"
-    message["target_car_type"] = "CHANGE THIS"
-    message["target_car_color"] = "CHANGE THIS"
-    message["message"] = check_message_appropriate(message["message"])
+    message["target_car_type"] = None
+    message["target_car_color"] = None
+
+    database_cars = ['honda', 'toyota', 'nissan', 'hyundai', 'ford', 'chevrolet']
+
+    database_color = ['red', 'yellow', 'blue', 'orange', 'green', 'violet', 'black'
+    'gray', 'white', 'silver', 'brown', 'beige', 'gold']
+
+    for car in database_cars:
+        if car in message:
+            message["target_car_type"]=car
+            break
+    #if time look for red car and if only one then good
+    if message["target_car_type"] != None:
+        for color in database_color:
+            if color in message:
+                message["target_car_color"] = color
+                break
+
+    message["target_plate_num"] =
+    db.find_name_plate(message["target_car_color"], message["target_car_type"]
+    message["longitude"], message["latitude"], message["nearby_cars"],
+    message["send_time"])
+
+    if message["target_plate_num"] = None:
+        flag_error=True
+    if not flag_error:
+        message["message"] = check_message_appropriate(message["message"])
     return message
 
 def check_message_appropriate(message):
-    sys.exit()
+    targetC = message["target_car_type"]
+    lenWordC = len(message["target_car_type"])
+    targetM = message["target_car_color"]
+    lenWordM = len(message["target_car_color"])
+    message_index=message.split(' ')
+    index1=message_index.index(targetC)
+    index2=message_index.index(targetM)
+    if index2==0 and index1>index2:
+        message=message[index1+lenWordC+1:len(message)]
+    else if index1==0 and index2>index1:
+        message=message[index2+lenWordM+1:len(message)]
+    string_final="Somone on the road advised you" + message
+    return string_final
